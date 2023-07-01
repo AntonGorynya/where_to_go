c31ea69f939d9ec32500e5e35b4a8d972e678207
Anton <antongorynya1@gmail.com>
2023-07-01 21:35:04 +0300

    add Image Inline fields to Place model

--- where_to_go/web_app/models.py
+++ where_to_go/web_app/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     description_short = models.TextField(default='Пару слов о месте', verbose_name='Краткое описание')
     description_long = models.TextField(default='Подробное описание места', verbose_name='Подробное Описание')
     placeId = models.CharField(max_length=50, verbose_name='placeId')
-    detailsUrl = models.CharField(max_length=50, verbose_name='detailsUrl')
+    #detailsUrl = models.CharField(max_length=50, verbose_name='detailsUrl')
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
 
@@ -19,17 +19,22 @@ class Image(models.Model):
     name = models.ImageField('Изображение', null=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='place')
 
-    @classmethod
-    def validate_number(cls, image):
-        numbers = list(cls.objects.filter(place=image.place).values('number').distinct())
-        numbers = [num['number'] for num in numbers]
-        print(numbers)
-        print(image.number)
-        if image.number in numbers or image.number < 1:
-            raise forms.ValidationError('Номера должны отличаться и быть положительны')
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['place', 'number'], name='name of constraint')
+        ]
 
     def __str__(self):
         return f'{self.number} {self.place}'
 
-    def clean(self):
-        self.validate_number(self)
+    # @classmethod
+    # def validate_number(cls, image):
+    #     numbers = list(cls.objects.filter(place=image.place).values('number').distinct())
+    #     numbers = [num['number'] for num in numbers]
+    #     print(numbers)
+    #     print(image.number)
+    #     if image.number in numbers or image.number < 1:
+    #         raise forms.ValidationError('Номера должны отличаться и быть положительны')
+
+    # def clean(self):
+    #     self.validate_number(self)
