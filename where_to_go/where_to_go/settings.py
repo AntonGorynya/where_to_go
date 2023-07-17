ee6e499675ee77c482e41f602cc7037bb2572747
Anton <antongorynya1@gmail.com>
2023-07-17 12:35:39 +0300

    удалил закоментированный код

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'web_app', 'static')
 STATIC_ROOT = env('STATIC_ROOT')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
