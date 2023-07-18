773d56bbe43ae802d6463813b0bf59a6a137f6e7
Anton <antongorynya1@gmail.com>
2023-07-18 16:13:21 +0300

    Добавил format_html

--- where_to_go/map_app/admin.py
+++ where_to_go/map_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.utils.html import mark_safe
+from django.utils.html import mark_safe, format_html
 from .models import Place, Image
 from adminsortable2.admin import SortableAdminMixin, SortableTabularInline
 
@@ -12,12 +12,7 @@ class ImageAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ['place', 'order', 'preview']
 
     def preview(self, obj):
-        return mark_safe('<img src="{url}" style="max-height:200px">'.format(
-            url=obj.image.url,
-            width=obj.image.width,
-            height=obj.image.height,
-        )
-        )
+        return format_html('<img src="{url}" style="max-height:200px">', url=obj.image.url)
 
 
 class ImageInline(SortableTabularInline):
@@ -27,12 +22,7 @@ class ImageInline(SortableTabularInline):
     ]
 
     def preview(self, obj):
-        return mark_safe('<img src="{url}" style="max-height:200px">'.format(
-            url=obj.image.url,
-            width=obj.image.width,
-            height=obj.image.height,
-        )
-        )
+        return format_html('<img src="{url}" style="max-height:200px">', url=obj.image.url)
 
 
 @admin.register(Place)
